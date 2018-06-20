afe3bf1725f73433ccf04572cf86cffd3368d642
Margarita Noche <Margarita@users-MBP.localdomain>
2018-06-20 12:30:31 -0700

    find missing number

--- missing_number.py
+++ missing_number.py
@@ -7,3 +7,14 @@ def missing_number(nums_list, length):
 
 
 print missing_number([2, 1, 4, 3, 6, 5, 7, 10, 9], 10)
+
+
+def missing_number_2(nums_list, length):
+    """Returns the missing number from the list."""
+    # Runtime O(n log n)
+    for i in range(length):
+        nums_list.sort()
+        nums_list[i] != i + 1
+
+
+print missing_number([2, 1, 4, 3, 6, 5, 7, 10, 9], 10)
