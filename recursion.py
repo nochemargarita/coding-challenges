089d45df18e8a48f34855b8c324fe57738ce5a5f
Margarita Noche <Margarita@users-MBP.attlocal.net>
2018-06-26 13:48:15 -0700

    print each item on the list

--- recursion.py
+++ recursion.py
@@ -6,5 +6,15 @@ def count_recursively(lst):
     return 0
     
 
-print count_recursively([])
-print count_recursively([5, 6, 7])
+# print count_recursively([])
+# print count_recursively([5, 6, 7])
+
+
+def print_recursively(lst):
+    """Print items in the list, using recursion."""
+
+    if lst:
+        print lst[0]
+        return print_recursively(lst[1:])
+
+print_recursively([6, 2, 3])
