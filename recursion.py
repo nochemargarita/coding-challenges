2bc47f492402a3bdb0ed400724bfa35a7138fd1a
Margarita Noche <Margarita@users-MBP.attlocal.net>
2018-06-26 14:27:43 -0700

    get index location of an item in a list.

--- recursion.py
+++ recursion.py
@@ -4,7 +4,6 @@ def count_recursively(lst):
     if lst:
         return 1 + count_recursively(lst[1:])
     return 0
-    
 
 # print count_recursively([])
 # print count_recursively([5, 6, 7])
@@ -17,4 +16,31 @@ def print_recursively(lst):
         print lst[0]
         return print_recursively(lst[1:])
 
-print_recursively([6, 2, 3])
+# print_recursively([6, 2, 3])
+
+
+def recursive_index(word, lst):
+    """Given list (haystack), return index (0-based) of needle in the list.
+
+    Return None if needle is not in haystack.
+
+    Do this with recursion. You MAY NOT USE A `for` OR `while` LOOP.
+    """
+
+    index = 0
+    def inner(word, lst, index):
+        if index < len(lst):
+            if lst[index] == word:
+                return index
+
+            index += 1
+            return inner(word, lst, index)
+
+    return inner(word, lst, index)
+
+lst = ["hey", "there", "you"]
+
+# print recursive_index('hey', lst)
+# print recursive_index('there', lst)
+# print recursive_index('you', lst)
+# print recursive_index('how', [])
