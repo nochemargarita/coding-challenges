72a8708b725623c0b2467a20960f075215ecea1e
Margarita Noche <Margarita@users-MBP.localdomain>
2018-06-28 15:23:54 -0700

    Optimize solution

--- reverse_list_of_chars.py
+++ reverse_list_of_chars.py
@@ -1,11 +1,24 @@
+# def reverse(list_of_chars):
+#     # Reverse the input list of chars in place
+#     for i in range(len(list_of_chars)):  # O(n)
+#         list_of_chars.insert(i, list_of_chars.pop())  # insert O(n), pop O(1)
+
+#     return list_of_chars
+
+
+# O(n^2)
 def reverse(list_of_chars):
     # Reverse the input list of chars in place
-    for i in range(len(list_of_chars)):
-        list_of_chars.insert(i, list_of_chars.pop())
-        
+    if list_of_chars:
+        left = 0  # O(1) space
+        while left < len(list_of_chars)/2 + 1:  # O(n)
+            list_of_chars[left], list_of_chars[-(left + 1)] = list_of_chars[-(left + 1)], list_of_chars[left]
+
+            left += 1
+
     return list_of_chars
 
-
-print reverse([])
-print reverse(['A', 'B', 'C', 'D', 'E'])
-print reverse(['A'])
+# O(n)
+# print reverse([])
+# print reverse(['A', 'B', 'C', 'D', 'E'])
+# print reverse(['A'])
