da1b0d725b7dac009b9f5f5e1f57c0f0ba6c43d4
Margarita Noche <nochemargarita@gmail.com>
2018-07-17 22:14:42 -0700

    other edge case handled like having a different character.

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -10,6 +10,12 @@ def is_balanced_brackets(s):
     >>> is_balanced_brackets('{][]}')
     False
 
+    >>> is_balanced_brackets('')
+    True
+
+    >>> is_balanced_brackets('[a]')
+    True
+
     """
 
     brackets = {'}': '{',
@@ -21,12 +27,12 @@ def is_balanced_brackets(s):
     visited = []
     # O(n) space
 
-    for i in s: # O(n) time
+    for i in s:  # O(n) time
         if i in open_brackets:  # O(1) time
             visited.append(i)
-        elif i in brackets and brackets[i] == visited[-1]: # O(1) time
+        elif i in brackets and brackets[i] == visited[-1]:  # O(1) time
             visited.pop()
-        else:
+        elif i in brackets and brackets[i] != visited[-1]:
             return False
 
     return len(visited) == 0
