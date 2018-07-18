59219a991f78c1d5a00b91412d53519956bde224
Margarita Noche <nochemargarita@gmail.com>
2018-07-17 21:47:55 -0700

    Balanced completed but theres still probably a better solution. Not all edge cases are checked. Currently passed 3 tests.

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -15,15 +15,21 @@ def is_balanced_brackets(s):
     brackets = {'}': '{',
                 ']': '[',
                 ')': '('}
-
-    open_brackets = brackets.values()
-
+    # O(n) space
+    open_brackets = set(brackets.values())
+    # O(n) space
     visited = []
+    # O(n) space
 
-    for i in s:
-        if i in open_brackets:
+    for i in s: # O(n) time
+        if i in open_brackets:  # O(1) time
             visited.append(i)
-        elif i in brackets and i == visited[-1]:
+        elif i in brackets and brackets[i] == visited[-1]: # O(1) time
             visited.pop()
+        else:
+            return False
 
     return len(visited) == 0
+
+# O(n) total run time
+# O(n) space - need to clarify
