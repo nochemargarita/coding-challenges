82b3ecd2de05cb4be656905c687eaa29539bc3e8
Margarita Noche <nochemargarita@gmail.com>
2018-07-31 22:57:41 -0700

    Did not finish the problem yet, checking if tree is balanced or not. but some tests have passed

---
+++ check_balance.py
@@ -0,0 +1,136 @@
+"""Is the tree at this node balanced?
+
+To make this a bit more readable, let's alias our class name:
+
+    >>> N = BinaryNode
+
+For a tree of 1 item:
+
+    >>> tree1 = N(1)
+    >>> tree1.is_balanced()
+    True
+
+For a tree of 2 items:
+
+  1
+ /
+2
+
+    >>> tree2 = N(1,
+    ...           N(2))
+    >>> tree2.is_balanced()
+    True
+
+Three:
+
+  1
+ / \
+2   3
+
+    >>> tree3 = N(1,
+    ...           N(2), N(3))
+    >>> tree3.is_balanced()
+    True
+
+Four:
+
+     1
+    / \
+   2   4
+  /
+ 3
+
+    >>> tree4 = N(1,
+    ...           N(2,
+    ...             N(3)),
+    ...           N(4))
+    >>> tree4.is_balanced()
+    True
+
+Five:
+
+     1
+   /---\
+  2     5
+ / \
+3   4
+
+    >>> tree5 = N(1,
+    ...           N(2,
+    ...             N(3), N(4)),
+    ...           N(5))
+    >>> tree5.is_balanced()
+    True
+
+Imbalanced Four:
+
+    1
+   /
+  2
+ / \
+3   4
+
+    >>> tree4i = N(1,
+    ...            N(2,
+    ...              N(3), N(4)))
+    >>> tree4i.is_balanced()
+    False
+
+Imbalanced Six:
+
+    1
+   / \
+  2   6
+ / \
+3   4
+   /
+  5
+
+    >>> tree6i = N(1,
+    ...         N(2,
+    ...       N(3), N(4,
+    ...           N(5))),
+    ...                   N(6))
+    >>> tree6i.is_balanced()
+    False
+"""
+
+
+class BinaryNode(object):
+    """Node in a binary tree."""
+
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def is_balanced(self):
+        """Is the tree at this node balanced?"""
+
+        curr_l = self
+        curr_r = self
+
+        left = []
+        right = []
+
+        while curr_l.left:
+            left.append(curr_l.data)
+            curr_l = curr_l.left
+        
+        while curr_r.right:
+          right.append(curr_r.data)
+          curr_r = curr_r.right
+
+        if not left and right:
+            return False
+        else:
+            return True
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TEST PASSED! GO GO GO!\n")
