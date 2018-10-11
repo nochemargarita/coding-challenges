956868adf1c3f4e8948a658fda5c2ff05b841bd4
Margarita Noche <nochemargarita@gmail.com>
2018-10-10 21:12:25 -0700

    Balanced brackets solution handles other edge cases.

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -1,41 +1,81 @@
-def is_balanced_brackets(s):
-    """returns a boolean whether a string has balanced brackets.
+import unittest
 
-    >>> is_balanced_brackets('()')
-    True
 
-    >>> is_balanced_brackets('{[]}')
-    True
-
-    >>> is_balanced_brackets('{][]}')
-    False
-
-    >>> is_balanced_brackets('')
-    True
-
-    >>> is_balanced_brackets('[a]')
-    True
-
-    """
+def has_balanced_brackets(string):
+    """returns a boolean whether a string has balanced brackets."""
 
     brackets = {'}': '{',
                 ']': '[',
-                ')': '('}
-    # O(n) space
-    open_brackets = set(brackets.values())
-    # O(n) space
-    visited = []
-    # O(n) space
+                ')': '(',
+                '>': '<'}  # O(1) space
+    open_brackets = set(brackets.values())  # O(1) space # O(n) time
+    seen_open_brackets = []  # O(n) space
 
-    for i in s:  # O(n) time
-        if i in open_brackets:  # O(1) time
-            visited.append(i)
-        elif i in brackets and brackets[i] == visited[-1]:  # O(1) time
-            visited.pop()
-        elif i in brackets and brackets[i] != visited[-1]:
-            return False
+    for char in string:
+        if char in open_brackets:
+            seen_open_brackets.append(char)
 
-    return len(visited) == 0
+        elif char in brackets:
+            if seen_open_brackets == []:
+                return False
+            elif brackets[char] == seen_open_brackets[-1]:
+                seen_open_brackets.pop()
+            else:
+                return False
+
+    return seen_open_brackets == []
 
 # O(n) total run time
-# O(n) space - need to clarify
+# O(n) total space complexity
+
+class Test(unittest.TestCase):
+
+    def test_balanced_brackets_1(self):
+        actual = has_balanced_brackets('<ok>')
+        expected = True
+
+        self.assertEqual(actual, expected)
+
+    def test_balanced_brackets_2(self):
+        actual = has_balanced_brackets('<[ok]>')
+        expected = True
+
+        self.assertEqual(actual, expected)
+
+    def test_balanced_brackets_3(self):
+        actual = has_balanced_brackets('<[{(yay)}]>')
+        expected = True
+
+        self.assertEqual(actual, expected)
+
+    def test_balanced_brackets_4(self):
+        actual = has_balanced_brackets('<>[{(yay)}]>')
+        expected = False
+
+        self.assertEqual(actual, expected)
+
+    def test_balanced_brackets_5(self):
+        actual = has_balanced_brackets('<[{(yay<}]>')
+        expected = False
+
+        self.assertEqual(actual, expected)
+
+    def test_balanced_brackets_6(self):
+        actual = has_balanced_brackets('ok')
+        expected = True
+
+        self.assertEqual(actual, expected)
+
+    def test_balanced_brackets_7(self):
+        actual = has_balanced_brackets(' <   >')
+        expected = True
+
+        self.assertEqual(actual, expected)
+
+    def test_balanced_brackets_8(self):
+        actual = has_balanced_brackets('>()')
+        expected = False
+
+        self.assertEqual(actual, expected)
+
+unittest.main(verbosity=2)
